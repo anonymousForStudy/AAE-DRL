b591d4f4001030526a5fc68f6a8598a39158d8c1
NameDefault <opsai8484@gmail.com>
2025-03-04 14:36:21 +0100

    commented code

--- DRL/RL_tester.py
+++ DRL/RL_tester.py
@@ -1,18 +1,17 @@
+# libraries
 import csv
 import os
-
 import numpy as np
 import pandas as pd
 import torch
 import torch.utils.data
-
 import utils
 from DRL.EnvClass import Env
 from AAE import AAE_archi_opt
 from clfs import classifier
-
 from utils import RL_dataloader
 from DRL.RL import TD3
+# check for cuda and disable DSA
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ['TORCH_USE_CUDA_DSA'] = "1"
 
@@ -23,7 +22,7 @@ torch.manual_seed(0)
 class Tester(object):
     def __init__(self, test_loader, model_encoder, model_decoder, model_disc, classifier, in_out, discrete, actor_path,
                  critic_path):
-
+        # define test loader
         self.test_loader = RL_dataloader(test_loader)
 
         # self.expl_noise = 0.3
@@ -32,25 +31,27 @@ class Tester(object):
         self.discriminator = model_disc
         self.decoder = model_decoder
         self.classifier = classifier
-
+        # call enviroment in DRL/EnvClass
         self.env = Env(self.encoder, self.discriminator, self.classifier, self.decoder)
+        # call replay buffer in ./utils
         self.replay_buffer = utils.ReplayBuffer()
-
+        # set seed to 0
         torch.manual_seed(0)
         np.random.seed(0)
 
-        self.state_dim = in_out
-        self.action_dim = 7
-        self.discrete_features = discrete
-        self.max_action = 1
-        self.policy = TD3(self.state_dim, self.action_dim, self.discrete_features, self.max_action)
+        self.state_dim = in_out # 30 features
+        self.action_dim = 7 # 7 continuous action dim
+        self.discrete_features = discrete # 3 discrete action dim: tuple of unique values in each discrete feature => max action
+        self.max_action = 1 # max continuous action 
+        # define TD3+DDQN network
+        self.policy = TD3DDQN(self.state_dim, self.action_dim, self.discrete_features, self.max_action)
 
         self.continue_timesteps = 0
+        # actor state dictionary
         self.actor_path = actor_path
+        # critic state dictionary
         self.critic_path = critic_path
         self.evaluations = []
-
-
 
     def evaluate(self):
         episode_num = 0
